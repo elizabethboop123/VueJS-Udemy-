6b1ce0ee0ef6d6dc868d80ba526c4e2fa4e9ec49
Elizabeth Silvano <elizabeths@elizabeths.itflex.lan>
2021-04-05 16:55:14 -0300

    Seção 10 e 11

---
+++ Seção-6-Vue-cli/projeto-simples2/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+      {{ titulo }}
+      <button @click="titulo += '#'">Alterar</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            titulo: 'Teste Data usando Vue JS!!'
+        }
+    }
+}
+</script>
+
+<style>
+    #app {
+        background-color: rgb(30, 210, 210);
+        color: rgb(54, 10, 10);
+    }
+</style>
